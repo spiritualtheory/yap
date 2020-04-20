6dcb487d13f39b29fa3976f5c27c9130a0832fc5
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2020-04-19 17:12:38 -0700

    use signalwire instead of twilio

--- endpoints/_includes/twilio-client.php
+++ endpoints/_includes/twilio-client.php
@@ -1,9 +1,11 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
-use Twilio\Rest\Client;
+//use Twilio\Rest\Client;
+use SignalWire\Rest\Client;
 
 try {
-    $twilioClient = new Client(setting("twilio_account_sid"), setting("twilio_auth_token"));
+//    $twilioClient = new Client(setting("twilio_account_sid"), setting("twilio_auth_token"));
+    $twilioClient = new Client(setting("twilio_account_sid"), setting("twilio_auth_token"), array("signalwireSpaceUrl" => "northstarna.signalwire.com"));
     $GLOBALS['twilioClient'] = $twilioClient;
 } catch (\Twilio\Exceptions\ConfigurationException $e) {
     error_log("Missing Twilio Credentials");
